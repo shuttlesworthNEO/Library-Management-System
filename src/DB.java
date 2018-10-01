52f95495f7d348add185d174a1b13b7cbaba7670
Piyush Ajmani <piyushajmani5@gmail.com>
2018-10-01 22:33:08 +0530

    Fixed DB.java

--- src/DB.java
+++ src/DB.java
@@ -8,8 +8,6 @@ public class DB {
 			String url = "jdbc:mysql://localhost/MYDB";
 			Class.forName ("com.mysql.jdbc.Driver").newInstance ();
 			con = DriverManager.getConnection (url, "root", "bemyfrnd");
-//			Class.forName("com.mysql.jdbc.Driver");
-//			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/MYDB","root","8U+uMk?VDEk2");
 		}catch(Exception e){System.out.println(e);}
 		return con;
 	}
